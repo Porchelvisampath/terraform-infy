03fe4a99d34503c163a414f18c9c191d073acf26
Porchelvisampath <102731019+Porchelvisampath@users.noreply.github.com>
2026-01-08 10:39:39 +0530

    Create main.tf

---
+++ terraform-http/modules/compute/main.tf
@@ -0,0 +1,39 @@
+# Compute Engine VM (private)
+
+resource "google_compute_instance" "web_server" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  allow_stopping_for_update = true
+
+  tags = ["web-server"]
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnet_id
+    # No external IP → private VM
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
+}
+
+
+# Instance group for LB
+
+resource "google_compute_instance_group" "web_group" {
+  name      = "web-group"
+  zone      = var.zone
+  instances = [google_compute_instance.web_server.id]
+}
